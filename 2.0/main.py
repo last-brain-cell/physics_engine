68c673b4b039edf5631e2472b3a20c5586f0f916
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-04-25 00:15:55 +0530

    syntx errors

--- 2.0/main.py
+++ 2.0/main.py
@@ -1,5 +1,3 @@
-import os
-
 import pygame
 import math
 import random
@@ -8,7 +6,7 @@ pygame.init()
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
-GRAVITY = 9.8
+GRAVITY = 0.098
 FPS = 60
 colors = [
     (255, 0, 0),  # red
@@ -30,28 +28,30 @@ screen.fill((0, 0, 0))
 
 
 class Particle:
-    def __init__(self, radius, mass, x, y):
+    def __init__(self, radius, mass, x, y, initial_x_velocity=5, initial_y_velocity=0):
         self.mass = mass
-        self.velocity = [0, 0]
+        self.velocity = [initial_x_velocity, initial_y_velocity]
         self.radius = radius
         self.x = x
         self.y = y
         self.color = random.choice(colors)
 
-    def apply_force(self, force: list):
-        acceleration = [0, 0]
-        acceleration = [force[0] / self.mass, force[1] / self.mass]
-        self.velocity[0] += acceleration[0]
-        self.velocity[1] += acceleration[1]
+    def apply_gravity(self):
+        self.velocity[1] += GRAVITY
+
+    def apply_friction_x(self, friction: float = 0.99):
+        self.velocity[0] *= friction
 
     def update_position(self):
+        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 10)
+        print(self.velocity[0])
         self.x += self.velocity[0]
         self.y += self.velocity[1]
 
     def render(self, screen):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
 
-    def collision(self):
+    def stay_in_window(self):
         dist_right = SCREEN_WIDTH - self.x
         dist_left = self.x
         dist_bottom = SCREEN_HEIGHT - self.y
@@ -76,20 +76,26 @@ try:
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if pygame.mouse.get_pressed()[0]:
                     pos = pygame.mouse.get_pos()
-                    particles.append(Particle(20, 50, pos[0], pos[1]))
+                    particles.append(Particle(20, 50, pos[0], pos[1], initial_x_velocity=random.randint(1, 6)))
 
         screen.fill((0, 0, 0))
 
-        for particle in particles:
-            particle.apply_force([2, GRAVITY])
-            particle.update_position()
-            particle.collision()
-            particle.render(screen)
+        for i in range(len(particles)):
+            particles[i].apply_gravity()
+            particles[i].apply_friction_x(0.99)
+            particles[i].update_position()
+            particles[i].stay_in_window()
+            for j in range(len(particles)):
+                if j == i:
+                    continue
+
+
+            particles[i].render(screen)
 
         # Console:
         print(f"Particles: {len(particles)}")
 
-        pygame.display.flip()
+        pygame.display.update()
 
 except KeyboardInterrupt:
     pygame.quit()
--- test.py
+++ test.py
@@ -1,75 +1,181 @@
+# import pygame
+# import pandas as pd
+#
+# # initialize Pygame
+# pygame.init()
+#
+# # set the screen size
+# screen_width = 400
+# screen_height = 400
+# FPS = 60
+# screen = pygame.display.set_mode((screen_width, screen_height))
+#
+# # set the particle position
+# particle_x = screen_width // 2
+# particle_y = 20
+#
+# # set the particle radius and color
+# particle_radius = 20
+# particle_color = (255, 255, 255)
+#
+# # set the gravity and speed of the particle
+# gravity = 0.098
+# particle_speed = 0
+# click_velocity = 10
+#
+# # set the game loop
+# running = True
+# velocity_data = [0]
+#
+# clock = pygame.time.Clock()
+#
+# while running:
+#     dt = clock.tick(FPS) / 60
+#
+#     # fill the screen with black color
+#     screen.fill((0, 0, 0))
+#
+#     # draw the particle
+#     pygame.draw.circle(screen, particle_color, (particle_x, particle_y), particle_radius)
+#
+#     # check if the particle goes off the screen
+#     if particle_y < screen_height - particle_radius:
+#         particle_speed += gravity * dt
+#         particle_y += particle_speed
+#     elif particle_x > screen_width - particle_radius:
+#         particle_x = screen_width - particle_radius
+#     elif screen_width < 20:
+#         particle_x = 20
+#     else:
+#         particle_speed = 0
+#         particle_y = screen_height - particle_radius
+#
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+#         elif event.type == pygame.MOUSEBUTTONDOWN:
+#             if pygame.mouse.get_pressed()[0]:
+#                 particle_x, particle_y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
+#                 particle_speed = 0
+#                 print("Left Click Position" + str(pygame.mouse.get_pos()))
+#
+#     if particle_speed != 0:
+#         print("Particle Speed: " + str(particle_speed))
+#         velocity_data.append(particle_speed)
+#
+#     # update the screen
+#
+#     # print((particle_x, particle_y))
+#     pygame.display.update()
+#
+# data = pd.DataFrame(velocity_data)
+# data.to_csv("velocity_data.txt", index=False)
+#
+# # quit Pygame
+# pygame.quit()
+
 import pygame
-import pandas as pd
+import math
 
-# initialize Pygame
-pygame.init()
+wScreen = 1200
+hScreen = 500
 
-# set the screen size
-screen_width = 400
-screen_height = 400
-FPS = 60
-screen = pygame.display.set_mode((screen_width, screen_height))
+win = pygame.display.set_mode((wScreen,hScreen))
+pygame.display.set_caption('Projectile Motion')
 
-# set the particle position
-particle_x = screen_width // 2
-particle_y = 20
 
-# set the particle radius and color
-particle_radius = 20
-particle_color = (255, 255, 255)
+class ball(object):
+    def __init__(self,x,y,radius,color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
 
-# set the gravity and speed of the particle
-gravity = 0.098
-particle_speed = 0
-click_velocity = 10
+    def draw(self, win):
+        pygame.draw.circle(win, (0,0,0), (self.x,self.y), self.radius)
+        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius-1)
 
-# set the game loop
-running = True
-velocity_data = [0]
 
+    @staticmethod
+    def ballPath(startx, starty, power, ang, time):
+        angle = ang
+        velx = math.cos(angle) * power
+        vely = math.sin(angle) * power
+
+        distX = velx * time
+        distY = (vely * time) + ((-4.9 * (time ** 2)) / 2)
+
+        newx = round(distX + startx)
+        newy = round(starty - distY)
+
+
+        return (newx, newy)
+
+
+def redrawWindow():
+    win.fill((64,64,64))
+    golfBall.draw(win)
+    pygame.draw.line(win, (0,0,0),line[0], line[1])
+    pygame.display.update()
+
+def findAngle(pos):
+    sX = golfBall.x
+    sY = golfBall.y
+    try:
+        angle = math.atan((sY - pos[1]) / (sX - pos[0]))
+    except:
+        angle = math.pi / 2
+
+    if pos[1] < sY and pos[0] > sX:
+        angle = abs(angle)
+    elif pos[1] < sY and pos[0] < sX:
+        angle = math.pi - angle
+    elif pos[1] > sY and pos[0] < sX:
+        angle = math.pi + abs(angle)
+    elif pos[1] > sY and pos[0] > sX:
+        angle = (math.pi * 2) - angle
+
+    return angle
+
+
+golfBall = ball(300,494,5,(255,255,255))
+
+run = True
+time = 0
+power = 0
+angle = 0
+shoot = False
 clock = pygame.time.Clock()
+while run:
+    clock.tick(200)
+    if shoot:
+        if golfBall.y < 500 - golfBall.radius:
+            time += 0.05
+            po = ball.ballPath(x, y, power, angle, time)
+            golfBall.x = po[0]
+            golfBall.y = po[1]
+        else:
+            shoot = False
+            time = 0
+            golfBall.y = 494
 
-while running:
-    dt = clock.tick(FPS) / 60
-
-    # fill the screen with black color
-    screen.fill((0, 0, 0))
-
-    # draw the particle
-    pygame.draw.circle(screen, particle_color, (particle_x, particle_y), particle_radius)
-
-    # check if the particle goes off the screen
-    if particle_y < screen_height - particle_radius:
-        particle_speed += gravity * dt
-        particle_y += particle_speed
-    elif particle_x > screen_width - particle_radius:
-        particle_x = screen_width - particle_radius
-    elif screen_width < 20:
-        particle_x = 20
-    else:
-        particle_speed = 0
-        particle_y = screen_height - particle_radius
+    line = [(golfBall.x, golfBall.y), pygame.mouse.get_pos()]
+    redrawWindow()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]:
-                particle_x, particle_y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
-                particle_speed = 0
-                print("Left Click Position" + str(pygame.mouse.get_pos()))
+            run = False
 
-    if particle_speed != 0:
-        print("Particle Speed: " + str(particle_speed))
-        velocity_data.append(particle_speed)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if not shoot:
+                x = golfBall.x
+                y = golfBall.y
+                pos =pygame.mouse.get_pos()
+                shoot = True
+                power = math.sqrt((line[1][1]-line[0][1])**2 +(line[1][0]-line[0][1])**2)/8
+                angle = findAngle(pos)
 
-    # update the screen
 
-    # print((particle_x, particle_y))
-    pygame.display.update()
 
-data = pd.DataFrame(velocity_data)
-data.to_csv("velocity_data.txt", index=False)
-
-# quit Pygame
 pygame.quit()
+quit()
